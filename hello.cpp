fe69424d786274f3196a15eeb05e177c1017e6ca
SteelMatrix <jpschool232@gmail.com>
2019-08-26 12:41:51 -0600

    Added Dog.jpg, Makefile, hello.cpp, and hello.md

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+int main()
+{
+	std::cout << "# John" << std::endl << "Hello! My name is John," << std::endl << "I am from New Orleans, Louisiana. I moved here in 2005, and have lived here since then." << std::endl;
+	std::cout << "In this class, I am hoping to become more fluent in the C++ programming language. I currently know (but am __still__ learning) the following languages:" << std::endl;
+	std::cout << "* Assembly" << std::endl;
+	std::cout << "* Batch" << std::endl;
+	std::cout << "* C" << std::endl;
+	std::cout << "* C#" << std::endl;
+	std::cout << "* **Python**" << std::endl;
+	std::cout << "  * I am currently working on making a video game in this language, to improve my general programming logic skill. :space_invader: :video_game:" << std::endl;
+	std::cout << "* VHDL" << std::endl;
+	std::cout << "  * I am not currently programming in this language, but I enjoyed programming in it when I did." << std::endl;
+	std::cout << "I have two pets, but unfortunately I only have a photo of one. His name is Scout, and he is a Jack Russel Terrier mix:" << std::endl;
+	std::cout << "![Image of Scout, a small white and brown dog](Dog.jpg)" << std::endl;
+	std::cout << "After graduating from the University of New Mexico, I might find a job here for a short time, then take some time to travel the world. Other than that, we'll see what's next! :earth_americas: :airplane:" << std::endl;
+	return 0;
+}
---
+++ hello.md
@@ -0,0 +1,15 @@
+# John
+Hello! My name is John,
+I am from New Orleans, Louisiana. I moved here in 2005, and have lived here since then.
+In this class, I am hoping to become more fluent in the C++ programming language. I currently know (but am __still__ learning) the following languages:
+* Assembly
+* Batch
+* C
+* C#
+* **Python**
+  * I am currently working on making a video game in this language, to improve my general programming logic skill. :space_invader: :video_game:
+* VHDL
+  * I am not currently programming in this language, but I enjoyed programming in it when I did.
+I have two pets, but unfortunately I only have a photo of one. His name is Scout, and he is a Jack Russel Terrier mix:
+![Image of Scout, a small white and brown dog](Dog.jpg)
+After graduating from the University of New Mexico, I might find a job here for a short time, then take some time to travel the world. Other than that, we'll see what's next! :earth_americas: :airplane:
