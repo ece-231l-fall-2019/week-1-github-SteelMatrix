4a8876a6915725010871d70f49ec37dad42b5825
SteelMatrix <jpschool232@gmail.com>
2019-08-27 13:02:52 -0600

    Edited some bold text to be italic.

--- hello.cpp
+++ hello.cpp
@@ -7,7 +7,7 @@ int main()
 	std::cout << "## What I hope to get from this class" << std::endl;
 	std::cout << "In this class, I am hoping to become more fluent in the C++ programming language. I have programmed some in C++, but I am not skilled at all in the language. I am much more effective in programming in other languages, but taking this course will help me learn another." << std::endl;
 	std::cout << "## My current programming skill" << std::endl;
-	std::cout << "I currently know (but am __still__ learning) the following languages:" << std::endl;
+	std::cout << "I currently know (but am *still* learning) the following languages:" << std::endl;
 	std::cout << "* Assembly" << std::endl;
 	std::cout << "* Batch" << std::endl;
 	std::cout << "* C" << std::endl;
@@ -19,7 +19,7 @@ int main()
 	std::cout << "## A little more about me" << std::endl;
 	std::cout << "I have two pets, but unfortunately I only have a photo of one, currently. His name is Scout, and he is a Jack Russel Terrier mix:" << std::endl << std::endl <<std::endl;
 	std::cout << "![Image of Scout, a small white and brown dog](Dog.jpg)" << std::endl << std::endl << std::endl;
-	std::cout << "I have played trumpet for a few years, and still do every once in a while. I also enjoy composing music, animating, reading, and __sometimes__ I enjoy doing homework." << std::endl;
+	std::cout << "I have played trumpet for a few years, and still do every once in a while. I also enjoy composing music, animating, reading, and *sometimes* I enjoy doing homework." << std::endl;
 	std::cout << "## My plans for the future" << std::endl;
 	std::cout << "After graduating from the University of New Mexico, I might find a job here for a short time, then take some time to travel the world. Other than that, we'll see what's next! :earth_americas: :airplane:" << std::endl;
 	return 0;
--- hello.md
+++ hello.md
@@ -5,7 +5,7 @@ Hello! My name is John, and I am studying for computer engineering. I am origina
 ## What I hope to get from this class
 In this class, I am hoping to become more fluent in the C++ programming language. I have programmed some in C++, but I am not skilled at all in the language. I am much more effective in programming in other languages, but taking this course will help me learn another.
 ## My current programming skill
-I currently know (but am __still__ learning) the following languages:
+I currently know (but am *still* learning) the following languages:
 * Assembly
 * Batch
 * C
@@ -21,6 +21,6 @@ I have two pets, but unfortunately I only have a photo of one, currently. His na
 ![Image of Scout, a small white and brown dog](Dog.jpg)
 
 
-I have played trumpet for a few years, and still do every once in a while. I also enjoy composing music, animating, reading, and __sometimes__ I enjoy doing homework.
+I have played trumpet for a few years, and still do every once in a while. I also enjoy composing music, animating, reading, and *sometimes* I enjoy doing homework.
 ## My plans for the future
 After graduating from the University of New Mexico, I might find a job here for a short time, then take some time to travel the world. Other than that, we'll see what's next! :earth_americas: :airplane:
